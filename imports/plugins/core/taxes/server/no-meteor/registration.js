6d27bc865e87ae31357241a81f89e2d0b29c6b3f
Seun Martins <seundotme@gmail.com>
2018-12-12 22:28:43 +0100

    feat: Setup getTaxServicesForShop() to return  both active and fallback taxServices

--- imports/plugins/core/taxes/server/no-meteor/registration.js
+++ imports/plugins/core/taxes/server/no-meteor/registration.js
@@ -19,18 +19,25 @@ export function registerPluginHandler({ name: pluginName, taxServices: pluginTax
  * @returns {Object|null} The definition from registerPackage for the tax service that is
  *   currently enabled for the shop with ID `shopId`
  */
-export async function getActiveTaxServiceForShop(context, shopId) {
+export async function getTaxServicesForShop(context, shopId) {
   const plugin = await context.collections.Packages.findOne({ name: "reaction-taxes", shopId });
   if (!plugin) return null;
 
-  const { activeTaxServiceName } = plugin.settings || {};
-  if (!activeTaxServiceName) return null;
+  const { activeTaxServiceName, fallbackTaxServiceName } = plugin.settings || {};
+  if (!activeTaxServiceName) return null; // at least an active must be set
 
-  const config = taxServices[activeTaxServiceName];
-  if (!config) {
+  const activeConfig = taxServices[activeTaxServiceName];
+  const fallbackConfig = taxServices[fallbackTaxServiceName];
+
+  if (!activeConfig) {
     throw new Error(`Active tax service is "${activeTaxServiceName}" but no such service exists. ` +
       "Did you forget to install the plugin that provides this service?");
   }
 
-  return config;
+  if (fallbackConfig && !fallbackConfig) {
+    throw new Error(`Fallback tax service is "${fallbackTaxServiceName}" but no such service exists. ` +
+      "Did you forget to install the plugin that provides this service?");
+  }
+
+  return { activeTaxService: activeConfig, fallbackTaxService: fallbackConfig };
 }
